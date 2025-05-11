fc5e62898523d5bc5f9649ed06d27176ea02f5a2
hienthan <hien.thanquang@gmail.com>
2025-05-11 15:24:15 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/sections/Hero";
+import {Layout} from "./components/Layout";
+
+const App = () => {
+  return (
+    <Layout title="Landing Page PJ">
+      <Hero />
+    </Layout>
+  );
+};
+
+export default App;
