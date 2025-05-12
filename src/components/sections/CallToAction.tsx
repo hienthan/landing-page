0866683dae96490e996fd0cf2cfc3efb434ca4d4
hienthan <hien.thanquang@gmail.com>
2025-05-12 12:45:41 +0700

    Fixed the CTA typo

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -9,10 +9,10 @@ export const CTA = () => {
         <div className="relative rounded-2xl overflow-hidden">
           <div className="relative z-10 mx-auto text-center max-w-xl md:max-w-2xl py-8 md:py-10 px-6 md:px-8">
             <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-heading-1">
-              Quick Start you
+              Quick Start your{" "}
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-violet-600">
                 own AI
-              </span>
+              </span>{" "}
               Business
             </h1>
             <Paragraph className="pt-10">
